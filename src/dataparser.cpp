878680c175dc40db831c03a072ce5c013f4879fd
JalilBNH <jalil.bnh13@gmail.com>
2025-11-03 16:49:00 +0100

    FORMAT dataparser

--- src/dataparser.cpp
+++ src/dataparser.cpp
@@ -1,24 +1,26 @@
 #include "aoc/dataparser.h"
 
-namespace aoc {
-std::tuple<std::vector<int>, std::vector<int>> twoColsParser(std::string inputPath)
+namespace aoc
 {
-    int num1, num2;
-    std::vector<int> list1, list2;
-    std::string inputLine;
-    std::ifstream inputFile(inputPath);
-
-    while (getline(inputFile, inputLine))
+    std::tuple<std::vector<int>, std::vector<int>> twoColsParser(std::string inputPath)
     {
-        num1 = stoi(inputLine.substr(0, inputLine.find(" ")));
-        num2 = stoi(inputLine.substr(inputLine.find(" ") + 3, inputLine.size() - 1));
+        int num1, num2;
+        std::vector<int> list1, list2;
+        std::string inputLine;
+        std::ifstream inputFile(inputPath);
 
-        list1.push_back(num1);
-        list2.push_back(num2);
+        while (getline(inputFile, inputLine))
+        {
+            num1 = stoi(inputLine.substr(0, inputLine.find(" ")));
+            num2 = stoi(inputLine.substr(inputLine.find(" ") + 3, inputLine.size() - 1));
+
+            list1.push_back(num1);
+            list2.push_back(num2);
+        }
+
+        inputFile.close();
+
+        return {list1, list2};
     }
-
-    inputFile.close();
-
-    return {list1, list2};
-}
+    
 }
